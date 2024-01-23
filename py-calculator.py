c53ee0ef5c36fc198e992c9586a426fa92831721
Balaji-02 <91966758+Balaji-02@users.noreply.github.com>
2024-01-23 12:41:13 +0530

    Updated py-calculator.py by adding power function (20MIC0108)

--- py-calculator.py
+++ py-calculator.py
@@ -5,3 +5,12 @@ def modulus(a,b):
 x=int(input("Enter the first integer num: "))
 y=int(input("Enter the second integer num: "))
 modulus(x,y)
+
+def power(m,n):
+    c=m**n
+    print("The modulus of two numbers are : ",c)    
+
+x=int(input("Enter the first integer num: "))
+y=int(input("Enter the second integer num: "))
+power(x,y)
+#done by Balaji.M
