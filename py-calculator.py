92bf4c9e2c0bb4f4dac4b0ab29b00f47f8c0c816
vak-01 <78302047+vak-01@users.noreply.github.com>
2024-01-23 12:35:24 +0530

    Create py-calculator.py

---
+++ py-calculator.py
@@ -0,0 +1,7 @@
+def modulus(a,b):
+    c=a%b
+    print("The modulus of two numbers are : ",c)    
+
+x=int(input("Enter the first integer num: "))
+y=int(input("Enter the second integer num: "))
+modulus(x,y)
